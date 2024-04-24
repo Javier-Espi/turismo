962675c62a59d6d33aa86e1f71c06adf3ac149f5
javier.espi <javier.marcelo.espi@gmail.com>
2024-04-23 23:19:35 -0300

    Se cambia datos fijos para tomarlos de un Json y se modifican algunos nombres carpetas para adecuarse mejor

--- statics/js/crud-alojamientos.js
+++ statics/js/crud-alojamientos.js
@@ -1,24 +1,69 @@
-const alojamientos = {
-    id: [],
-    imagen: [], 
-    cuit: [],
-    nombre: [],
-    web: [],
-    telefono: [],
-    correo: [],
-    direccion: [],
-    coordenadas: [],
-}
 
-alojamientos = {
-    id: [1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9],
-    imagen: ["Alojamiento.webp", "Alojamiento.webp", "Alojamiento.webp", "Alojamiento.webp", "Alojamiento.webp", "Alojamiento.webp", "Alojamiento.webp", "Alojamiento.webp", "Alojamiento.webp"], 
-    cuit: [20024393968, 20028938471, 20042507157, 20043573579, 20043817818, 20044390680, 20044705606, 20044775191, 20045445934],
-    nombre: ["La casa del Bosque 1", "La casa del Bosque 2222222222", , "La casa del Bosque 3", "La casa del Bosque 4", "La casa del Bosque 5", "La casa del Bosque 6", "La casa del Bosque 7", "La casa del Bosque 8", "La casa del Bosque 9" ],
-    web: ["google.com.ar", "google.com.ar", "google.com.ar", "google.com.ar", "google.com.ar", "google.com.ar", "google.com.ar", "google.com.ar", "google.com.ar", "google.com.ar" ],
-    telefono: [5555555551, 5555555552, 5555555553, 5555555554, 5555555555, 5555555556, 5555555557, 5555555558, 5555555559],
-    correo: ["bosque1@turismo.com.ar", "bosque2@turismo.com.ar", "bosque3@turismo.com.ar", "bosque4@turismo.com.ar", "bosque5@turismo.com.ar", "bosque6@turismo.com.ar", "bosque7@turismo.com.ar", "bosque8@turismo.com.ar", "bosque9@turismo.com.ar"],
-    direccion: ["calle 1 Numero 123", "calle 2 Numero 123", "calle 3 Numero 123", "calle 4 Numero 123", "calle 5 Numero 123", "calle 6 Numero 123", "calle 7 Numero 123", "calle 8 Numero 123", "calle 9 Numero 123"],
-    coordenadas: [ [-32.17222, -64.77706], [-32.16583, -64.77396], [-32.17100, -64.77914], [-32.17224, -64.77704], [-32.16995, -64.77566], [-32.17020, -64.77564], [-32.17167, -64.76835], [-32.17246, -64.77444], [-32.17177, -64.77120] ],
-}
+    function actualizarAlojamientoSeleccionadoPorId(id) {
+        // Encuentra el alojamiento por ID
+        fetch('../statics/data/alojamientos-crud.json')
+          .then(response => response.json())
+          .then(data => {
+            let alojamiento = data.find(aloj => aloj.id === id);
+            if (!alojamiento) {
+              throw new Error('Alojamiento no encontrado');
+            } else {
+                let alojamientoSeleccionado = document.querySelector('.alojamiento-selected');
+                let item = document.createElement('div');
+                item.className = 'alojamiento-selected-item';
+                item.innerHTML = `
+                    <div class="alojamiento-selected-item-principal">
+                        <img src="${alojamiento.imagen}" alt="" class="alojamiento-selected-item-principal-img">
+                        <p class="alojamiento-selected-item-principal-nombre"><b>${alojamiento.nombre}</b></p>
+                    </div>
+                    <div class="alojamiento-selected-item-detalle">
+                        <p class="alojamiento-selected-item-detalle-web">
+                            <a href="${alojamiento.web}" target ="_blank" rel="noopener noreferrer">Sitio Web</a>
+                        </p>
+                    <p class="alojamiento-selected-item-detalle-telefono">
+                    <a href="tel:${alojamiento.telefono}">${alojamiento.telefono}</a>
+                    </p>
+                    <p class="alojamiento-selected-item-detalle-email">
+                        <a href="${alojamiento.correo}">enviar mail al propietario</a>
+                    </p>
+                    <p class="alojamiento-selected-item-detalle-direccion">${alojamiento.direccion}</p>
+        `;
+        alojamientoSeleccionado.appendChild(item);
+        console.log(alojamiento.coordenadas)
+    }
+          })
+          .catch(error => console.error('Error al buscar el alojamiento:', error));
+        }
 
+actualizarAlojamientoSeleccionadoPorId(1)
+
+// Suponiendo que tienes un archivo 'alojamientos-crud.json' con la información de los alojamientos
+fetch('../statics/data/alojamientos-crud.json')
+
+  .then(response => response.json())
+  .then(data => {
+    const listaAlojamientos = document.querySelector('.alojamiento-lista');
+    data.forEach(alojamiento => {
+      const item = document.createElement('li');
+      item.className = 'alojamiento-lista-item';
+      item.innerHTML = `
+        <div class="alojamiento-item">
+          <img src="${alojamiento.imagen}" alt="" class="alojamiento-item-img">
+          <div class="alojamiento-item-detalle">
+            <p class="alojamiento-item-detalle-nombre">
+              <b>${alojamiento.nombre}</b>
+            </p>
+            <p class="alojamiento-item-detalle-web">
+              <a href="${alojamiento.web}" target ="_blank" rel="noopener noreferrer">Sitio Web</a>
+            </p>
+            <p class="alojamiento-item-detalle-telefono">
+              <a href="tel:${alojamiento.telefono}">${alojamiento.telefono}</a>
+            </p>
+          </div>
+        </div>
+      `;
+      listaAlojamientos.appendChild(item);
+      
+    });
+  })
+  .catch(error => console.error('Error al cargar lista de alojamientos:', error));
